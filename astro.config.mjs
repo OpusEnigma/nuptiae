68dbede6c2dc8d97746ceafcca03a754f27a57ea
Thomas Crepinge <thomas.crepinge.dev@gmail.com>
2026-01-26 15:39:03 +0100

    Add GitHub Actions workflow for Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://OpusEnigma.github.io',
+  base: '/nuptiae/',   // Le nom du repo
+});
