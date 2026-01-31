89a0743174b43b14fba1b455fea2d2a212319c19
Thomas Crepinge <thomas.crepinge.dev@gmail.com>
2026-01-31 12:58:49 +0100

    Improve the landing page without hero image and rituals

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://OpusEnigma.github.io',
   base: '/nuptiae/',   // Le nom du repo
+  devToolbar: {
+    enabled: false,
+  },
 });
